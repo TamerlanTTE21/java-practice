1246f063348c722c78dc02b3e9a44dfc9f2422a0
runtime-1 <runtimestudent2023@gmail.com>
2025-11-24 10:32:00 +0500

    polymorphism

---
+++ src/Polymorphism/classtask/AgeValidationRule.java
@@ -0,0 +1,13 @@
+package Polymorphism.classtask;
+
+public class AgeValidationRule extends ValidationRule<Byte> {
+    public AgeValidationRule(Byte age) {
+        super(age, "Возраст для подачи на ипотеку должен быть старше 18 лет");
+    }
+
+    @Override
+    public boolean isValid() {
+        return value >= 18;
+    }
+
+}
