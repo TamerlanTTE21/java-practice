57e60fd6fb054ba02317c1eef4b2573cc4ab62ad
runtime-1 <runtimestudent2023@gmail.com>
2025-12-26 10:31:53 +0500

    CT

---
+++ src/Set/classtask_1/Main.java
@@ -0,0 +1,36 @@
+package Set.classtask_1;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> allPurchases = List.of(
+                "яблоки",
+                "молоко",
+                "колбаса",
+                "огурцы",
+                "сок",
+                "хлеб",
+                "виноград",
+                "молоко",
+                "йогурт",
+                "хлеб",
+                "пельмени"
+        );
+
+        Set<String> uniquePurchases = new HashSet<>();
+        for (String str : allPurchases) {
+
+            uniquePurchases.add(str);
+        }
+
+
+// .
+
+        System.out.println(allPurchases.size()); // 11
+        System.out.println(uniquePurchases.size()); // 9
+    }
+}
---
+++ src/Set/classtask_2/Main.java
@@ -0,0 +1,36 @@
+package Set.classtask_2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> first = new HashSet<>();
+        first.add("Astana");
+        first.add("Almaty");
+        first.add("Kokshetau");
+        first.add("Taraz");
+
+        Set<String> second = new HashSet<>();
+        second.add("Shymkent");
+        second.add("Taraz");
+        second.add("Pavlodar");
+        second.add("Astana");
+        second.add("Almaty");
+
+        Set<String> common = new HashSet<>();
+// заполнить множество общими элементами
+        for (String str : first) {
+
+            if (second.contains(str)) {
+                common.add(str);
+            }
+
+        }
+
+
+
+
+        System.out.println(common); // [Taraz, Astana, Almaty]
+    }
+}
