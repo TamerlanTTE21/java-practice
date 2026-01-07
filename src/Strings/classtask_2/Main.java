1797ecc85ab5da91e81dd0e64f9b32b75971eceb
runtime-1 <runtimestudent2023@gmail.com>
2026-01-07 10:27:51 +0500

    CT

---
+++ src/Strings/classtask_2/Main.java
@@ -0,0 +1,11 @@
+package Strings.classtask_2;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Так много методов!".indexOf("м", 3) ==4);
+        System.out.println("Как их все запомнить?!".charAt(21)  =='?');
+        System.out.println("Шиншилла".lastIndexOf("и", 4)  ==4);
+    }
+}
---
+++ src/Strings/classtask_3/Main.java
@@ -0,0 +1,23 @@
+package Strings.classtask_3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(capitalize("hello")); // Hello
+        System.out.println(capitalize("hELLO")); // Hello
+        System.out.println(capitalize("HELLO")); // Hello
+        System.out.println(capitalize("HeLlO")); // Hello
+        System.out.println(capitalize("javascript")); // Hello
+    }
+
+    public static String capitalize(String str) {
+        String first = str.substring(0, 1).toUpperCase();
+
+        String last = str.substring(1).toLowerCase();
+
+        String result = first + last;
+
+
+        return result;
+    }
+
+}
