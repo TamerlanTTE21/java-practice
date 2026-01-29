586a26e1d399cdda4079ede91b6ae42d4f6325af
runtime-1 <runtimestudent2023@gmail.com>
2026-01-29 10:32:10 +0500

    CT

---
+++ src/Functional_Programming/CT_2/Main.java
@@ -0,0 +1,19 @@
+package Functional_Programming.CT_2;
+
+public class Main {
+    public static void main(String[] args) {
+        MathFunction mathFunction = (a, b) -> {
+            if (a > b) {
+                return a;
+        } else {
+                return b;
+            }
+        };
+        int result = mathFunction.findMax(6, 9);
+        System.out.println(result);
+    }
+
+}
+interface MathFunction {
+    int findMax(int a, int b);
+}
---
+++ src/Functional_Programming/classtask_1/Main.java
@@ -0,0 +1,20 @@
+package Functional_Programming.classtask_1;
+
+public class Main {
+    public static void main(String[] args) {
+        WordEditor wordEditor = str -> {
+            String first = str.substring(0,1).toUpperCase();
+            String second = str.substring(1).toLowerCase();
+            String result = first + second;
+            return result;
+            };
+        String result = wordEditor.operation("heLLO");
+        System.out.println(result);
+
+
+
+    }
+}
+interface WordEditor {
+    String operation (String str);
+}
