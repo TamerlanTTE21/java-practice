2d1586c9c8c40048948305c6dda17ff95f1ae911
runtime-1 <runtimestudent2023@gmail.com>
2026-02-06 10:29:59 +0500

    CT

--- src/Stream/hometask_1/HW3.java
+++ src/Stream/hometask_1/HW3.java
@@ -6,10 +6,10 @@ import java.util.function.Predicate;
 
 public class HW3 {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        List<Integer> result = numbers.stream().allMatch(x -> x == 0)
-                .toList();
-        System.out.println(result);
+//        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//        List<Boolean> result = numbers.stream().allMatch(x -> x > 0)
+//
+//        System.out.println(result);
 
 
     }
