e0b65b5b253eae801de1a07f33c2c245c87740c2
runtime-1 <runtimestudent2023@gmail.com>
2026-02-13 10:32:56 +0500

    CT

---
+++ src/HTTP/classtask_2/HelloHandler.java
@@ -0,0 +1,30 @@
+package HTTP.classtask_2;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+public class HelloHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String query = exchange.getRequestURI().getQuery();
+        System.out.println(query);
+        String[] params = query.split("&");
+
+        String[] firstParam = params[0].split("=");
+        String[] secondParam = params[1].split("=");
+
+        int first = Integer.parseInt(firstParam[1]) ;
+        int second = Integer.parseInt(secondParam[1]) ;
+
+        String str =first + " + "+ second + " = " + (first + second);
+
+        exchange.sendResponseHeaders(200, 0);
+
+        OutputStream os = exchange.getResponseBody();
+        os.write(str.getBytes());
+        os.close();
+    }
+}
---
+++ src/HTTP/classtask_2/Test.java
@@ -0,0 +1,15 @@
+package HTTP.classtask_2;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/calculate", new HelloHandler());
+        server.start();
+
+    }
+}
